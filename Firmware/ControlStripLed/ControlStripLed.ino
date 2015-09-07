a2f4c3f77c863b8f71f8d15abdc6f8edd2ea0f5d
Jose A. Luceño <jalucenyo@gmail.com>
2015-09-07 19:44:37 +0200

    Envio de mensaje para describir desde otros dispositivos.

---
+++ Firmware/ControlStripLed/ControlStripLed.ino
@@ -0,0 +1,194 @@
+/* This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*  <jalucenyo@gmail.com> 13 of August, 2015
+*  Jose A. Luceño
+*/
+#include <ESP8266WiFi.h>
+
+//#define DEBUG
+#define STRIP_PIN  2
+
+// Generally, you should use "unsigned long" for variables that hold time
+const long interval = 2000;              // interval at which to read sensor
+
+const char* ssid     = "YOUR WIFI SSID";
+const char* password = "PASSWORD WIFI";
+
+const char* publish_key = "pub-YOUR PUBLISH KEY";
+const char* suscribe_key = "sub-YOUR SUSCRIBE KEY";
+const char* secret_key = "sec-c-YOUR SECREY KEY";
+
+const char* channel_sensor_list = "sensors_list";
+const char* channel = "control_strip_001";
+const char* host = "pubsub.pubnub.com";
+const int httpPort = 80;
+
+//Ticker tasks;
+WiFiClient client;
+String timetoken = "0";
+int value = 0;
+
+void setup() {
+
+  pinMode(STRIP_PIN, OUTPUT);
+
+  Serial.begin(115200);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  delay(2000);
+
+  // Attach Ticker Tasks
+  //tasks.attach(2.0, heartbeatTask); // HeartBeatTask every 2s
+
+}
+
+
+void loop() {
+
+  #ifndef DEBUG
+  Serial.print("connecting to ");
+  Serial.println(host);
+  #endif
+  // Use WiFiClient class to create TCP connections
+  delay(10);
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  String suscribeUrl = "/subscribe";
+  suscribeUrl += "/";
+  suscribeUrl += suscribe_key;
+  suscribeUrl += "/";
+  suscribeUrl += channel;
+  suscribeUrl += "_value";
+  suscribeUrl += "/0";
+  suscribeUrl += "/";
+  suscribeUrl += timetoken;
+
+  Serial.print("Requesting URL: ");
+  Serial.println(suscribeUrl);
+
+  // This will send the request to the server
+  client.print(String("GET ") + suscribeUrl + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: keep-alive\r\n"+
+               "Accept: */*\r\n\r\n");
+               //"Connection: close\r\n\r\n");
+
+  delay(10);
+  while(client.connected()){
+      while(client.available()){
+
+        String line = client.readStringUntil('\r');
+        //Serial.println(line);
+
+        if(line.substring(1,3) == "[["){
+          if(timetoken == "0"){
+            timetoken = line.substring(6,23);
+            client.stop();
+
+            #ifdef DEBUG
+            Serial.println("First Step: ");
+            Serial.println(timetoken);
+            Serial.println(line);
+            #endif
+
+          }else{
+            if(line.length() > 2){
+              timetoken = line.substring(line.lastIndexOf(',')+2,line.lastIndexOf(']')-1);
+              int value = line.substring(line.lastIndexOf(',')-3,line.lastIndexOf(':')+2).toInt();
+              analogWrite(STRIP_PIN, value);
+              Serial.println(value);
+            }
+            client.stop();
+            #ifdef DEBUG
+            Serial.println("Second Step: ");
+            Serial.println(line);
+            Serial.println(timetoken);
+            #endif
+
+          }
+        }
+      }
+  }
+
+  #ifdef DEBUG
+  Serial.println();
+  Serial.println("closing connection");
+  #endif
+  client.stop();
+
+  /**
+   *  Send list of sensors.
+  **/
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  String sensorListUrl = "/publish";
+  sensorListUrl += "/";
+  sensorListUrl += publish_key;
+  sensorListUrl += "/";
+  sensorListUrl += suscribe_key;
+  sensorListUrl += "/";
+  sensorListUrl += secret_key;
+  sensorListUrl += "/";
+  sensorListUrl += channel_sensor_list;
+  sensorListUrl += "/0";
+  sensorListUrl += "/%22"+String(channel)+"%22";
+
+  Serial.print("Requesting URL: ");
+  Serial.println(sensorListUrl);
+
+  // This will send the request to the server
+  client.print(String("GET ") + sensorListUrl + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
+  delay(10);
+
+  // Read all the lines of the reply from server and print them to Serial
+  #ifdef DEBUG
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+
+  Serial.println();
+  Serial.println("closing connection");
+  #endif
+  client.stop();
+
+
+}
---
+++ Firmware/SensorTempHumidity/SensorTempHumidity.ino
@@ -0,0 +1,193 @@
+/* This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*  <jalucenyo@gmail.com> 13 of August, 2015
+*  Jose A. Luceño
+*/
+#include <ESP8266WiFi.h>
+#include <DHT.h>
+
+//#define DEBUG
+#define DHTTYPE DHT22
+#define DHTPIN  2
+
+DHT dht(DHTPIN, DHTTYPE, 11); // 11 works fine for ESP8266
+float humidity, temp_f;  // Values read from sensor
+
+// Generally, you should use "unsigned long" for variables that hold time
+const long interval = 2000;              // interval at which to read sensor
+
+const char* ssid     = "YOUR WIFI SSID";
+const char* password = "PASSWORD WIFI";
+
+const char* publish_key = "pub-YOUR PUBLISH KEY";
+const char* suscribe_key = "sub-YOUR SUSCRIBE KEY";
+const char* secret_key = "sec-c-YOUR SECREY KEY";
+
+const char* channel = "ambient_sensor_001";
+const char* channel_sensor_list = "sensors_list";
+const char* host = "pubsub.pubnub.com";
+const int httpPort = 80;
+
+// Generally, you should use "unsigned long" for variables that hold time
+unsigned long previousMillis = 0;        // will store last temp was read
+
+const char* streamId   = "....................";
+const char* privateKey = "....................";
+
+void setup() {
+
+  Serial.begin(115200);
+  delay(100);
+
+  dht.begin();           // initialize temperature sensor
+
+  // We start by connecting to a WiFi network
+  Serial.println("MAC: ");
+  Serial.println(WiFi.macAddress());
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+
+void loop() {
+  delay(interval);
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  gettemperature();       // read sensor
+
+  String publishUrl = "/publish";
+  publishUrl += "/";
+  publishUrl += publish_key;
+  publishUrl += "/";
+  publishUrl += suscribe_key;
+  publishUrl += "/";
+  publishUrl += secret_key;
+  publishUrl += "/";
+  publishUrl += channel;
+  publishUrl += "/0";
+  publishUrl += "/%22T:"+String((int)temp_f)+"-H:"+String((int)humidity)+"%22";
+
+  Serial.print("Requesting URL: ");
+  Serial.println(publishUrl);
+
+  // This will send the request to the server
+  client.print(String("GET ") + publishUrl + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
+  delay(10);
+
+  // Read all the lines of the reply from server and print them to Serial
+  #ifdef DEBUG
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+
+  Serial.println();
+  Serial.println("closing connection");
+  #endif
+  client.stop();
+  delay(100);
+
+  /**
+   *  Send list of sensors.
+  **/
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  String sensorListUrl = "/publish";
+  sensorListUrl += "/";
+  sensorListUrl += publish_key;
+  sensorListUrl += "/";
+  sensorListUrl += suscribe_key;
+  sensorListUrl += "/";
+  sensorListUrl += secret_key;
+  sensorListUrl += "/";
+  sensorListUrl += channel_sensor_list;
+  sensorListUrl += "/0";
+  sensorListUrl += "/%22"+String(channel)+"%22";
+
+  Serial.print("Requesting URL: ");
+  Serial.println(sensorListUrl);
+
+  // This will send the request to the server
+  client.print(String("GET ") + sensorListUrl + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
+  delay(10);
+
+  // Read all the lines of the reply from server and print them to Serial
+  #ifdef DEBUG
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+
+  Serial.println();
+  Serial.println("closing connection");
+  #endif
+  client.stop();
+
+
+}
+
+void gettemperature() {
+  // Wait at least 2 seconds seconds between measurements.
+  // if the difference between the current time and last time you read
+  // the sensor is bigger than the interval you set, read the sensor
+  // Works better than delay for things happening elsewhere also
+  unsigned long currentMillis = millis();
+
+  if(currentMillis - previousMillis >= interval) {
+    // save the last time you read the sensor
+    previousMillis = currentMillis;
+
+    // Reading temperature for humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
+    humidity = dht.readHumidity();          // Read humidity (percent)
+    temp_f = dht.readTemperature();     // Read temperature as Fahrenheit
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(humidity) || isnan(temp_f)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+  }
+}
