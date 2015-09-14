890c8fb3de0c75c5a8bbde4b66bfee187eb91d11
Jose A. Luceño <jalucenyo@gmail.com>
2015-09-14 19:46:47 +0200

    Descubrir sensores y guardar en la BD #3

---
+++ TestAndroidApp/app/src/main/java/com/jalcdeveloper/weaconapp/database/WeaconMapper.java
@@ -0,0 +1,24 @@
+package com.jalcdeveloper.weaconapp.database;
+
+import com.jalcdeveloper.weaconapp.weacon.WeaconNode;
+
+/**
+ * Created by jalucenyo on 14/9/15.
+ */
+public class WeaconMapper {
+
+    /**
+     * Mapping object WeaconNode to Sensor
+     * @param weacon
+     * @return
+     */
+    public static Sensor toSensor(WeaconNode weacon){
+
+        Sensor sensor = new Sensor();
+        sensor.set_canal(weacon.getChannel());
+        sensor.set_tipo(weacon.getType());
+        return sensor;
+
+    }
+
+}
--- TestAndroidApp/app/src/main/java/com/jalcdeveloper/weaconapp/ui/tv/MainTvActivity.java
+++ TestAndroidApp/app/src/main/java/com/jalcdeveloper/weaconapp/ui/tv/MainTvActivity.java
@@ -2,30 +2,49 @@ package com.jalcdeveloper.weaconapp.ui.tv;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.jalcdeveloper.weaconapp.R;
+import com.jalcdeveloper.weaconapp.database.WeaconMapper;
+import com.jalcdeveloper.weaconapp.database.WeaconsDbHelper;
+import com.jalcdeveloper.weaconapp.weacon.WeaconManager;
+import com.jalcdeveloper.weaconapp.weacon.WeaconNode;
 
-public class MainTvActivity extends Activity {
+public class MainTvActivity extends Activity
+        implements WeaconManager.WeaconListener {
+
+    private static final String TAG = MainTvActivity.class.getSimpleName();
+    private WeaconsDbHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_tv);
+
+        db = new WeaconsDbHelper(getApplicationContext());
+
+        // Start discovery weacons connected in wifi.
+        WeaconManager.startDiscovery(this);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onNewWeacon(WeaconNode weacon) {
+        Log.d(TAG, "onNewWeacon " + weacon.getChannel());
+        db.addSensor(WeaconMapper.toSensor(weacon));
+    }
+
+    @Override
+    public void onError(String message) {
+
     }
 }
